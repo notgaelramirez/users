2b409f011978e5eec946c529187e3b4d4138ae11
Gael Ramirez <gael1423@gmail.com>
2022-06-20 18:47:34 -0500

    xq no funciona me odio alguien máteme por favor

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,28 @@ import UsersList from './components/UsersList'
 function App() {
   let [users, setUsers] = useState()
   const URL = 'https://users-crud1.herokuapp.com/users/'
-
+  const {handleSubmit, register} = useForm()
+  
   const getData = () =>{
     axios.get(URL)
       .then(res => setUsers(res.data))
   }
 
   const submit = data =>{
-
     axios.post(URL, data)
       .then(res => console.log(res.data))
       .catch(err => console.log(err))
       .finally(() => getData())
   }
 
-  const {handleSubmit, register} = useForm()
+
+  const updateUser = newData =>{
+
+    axios.put(`https://users-crud1.herokuapp.com/users/${user.id}/`, newData)
+      .then(res => console.log(res.data))
+      .catch( err => console.log(err))
+      .finally(() => getData())
+  }
 
   useEffect(()=>{
     getData()
@@ -35,6 +42,36 @@ function App() {
       </header>
 
       <div className="container">
+        <div className="edit-zone">
+          <form onSubmit={updateUser} action="">
+            <div>
+              <label htmlFor="first_name">Name</label>
+              <input type="text" id='first_name' {...register('first_name')}/>
+              <label htmlFor="last_name">Lastname</label>
+              <input type="text" id='last_name' {...register('last_name')}/>
+            </div>
+
+            <div>
+              <label htmlFor="email-edit">Email</label>
+              <input type="text" id='email-edit' {...register('email')} />
+            </div>
+
+            <div>
+              <label htmlFor="password-edit">Password</label>
+              <input type="password" id='password-edit' {...register('password')} />
+            </div>
+
+            <div>
+              <label htmlFor="bday-edit">Birthday</label>
+              <input type="date" id='bday-edit' {...register('birthday')} />
+            </div>
+
+            <div>
+              <button className='btn-edit'>Submit changes</button>
+            </div>
+          </form>
+        </div>
+
         <div className='card-zone'>
            {
             users?.map(user=>(
--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -11,40 +11,22 @@ const UsersList = ({user, getData}) => {
       .catch(err => console.log(err))
       .finally(() => getData())
   }
-
-
-  const editUser = () =>{
-
-    let userUpdate = {
-      email:'prueba2@gmail.com',
-      password:'password',
-      first_name: 'Hectora',
-      last_name: 'Chaparro',
-
-    }
-
-    axios.put(`https://users-crud1.herokuapp.com/users/${user.id}/`, userUpdate)
-      .then(res => console.log(res.data))
-      .catch(err => console.log(err))
-      .finally(()=> getData())
-  }
-
   
 
   return (
-    <div className='user-card'>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-      <div className='data'>
-        <div className='user-name'><b>{user?.first_name} {user?.last_name}</b></div>
-        <div className='email'><small>{user?.email}</small></div>
-        <div className='birthday'>{user?.birthday} <span className="material-symbols-outlined">cake</span></div>
-      </div>
+      <div className='user-card'>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <div className='data'>
+          <div className='user-name'><b>{user?.first_name} {user?.last_name}</b></div>
+          <div className='email'><small>{user?.email}</small></div>
+          <div className='birthday'>{user?.birthday} <span className="material-symbols-outlined">cake</span></div>
+        </div>
 
-      <aside className='crud'>
-        <button onClick={deleteUser} className='delete'><span className="material-symbols-outlined">delete</span></button>
-        <button onClick={editUser}><span className="material-symbols-outlined">edit</span></button>
-      </aside>
-    </div>
+        <aside className='crud'>
+          <button onClick={deleteUser} className='delete'><span className="material-symbols-outlined">delete</span></button>
+          <button><span className="material-symbols-outlined">edit</span></button>
+        </aside>
+      </div>
   )
 }
 
